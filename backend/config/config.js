46f1b4cf5096d59573ed8a06cc7d56339250b0bb
KasaaziGW <kasaazi.gw@gmail.com>
2022-04-30 15:08:48 +0200

    Changes to the config.js file

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,5 +1,5 @@
-dbPassword = 'mongodb+srv://user:<password>@cluster0.1vxr7.mongodb.net/<database>?retryWrites=true&w=majority'
-
+// dbPassword = 'mongodb+srv://user:<password>@cluster0.1vxr7.mongodb.net/<database>?retryWrites=true&w=majority'
+dbPassword = 'mongodb+srv://SW_ARC_TEAM_2:SW_ARC_TEAM_2@busdb.3y5bt.mongodb.net/BusDB?retryWrites=true&w=majority'
 module.exports = {
     'secret': '<password>',
     mongoURI: dbPassword
